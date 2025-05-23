5c006e3d691244b3ffd620ea8bd01952b053c004
Patrik Luks <patrikluks@Patrik--MacBook-Air.local>
2025-05-23 12:00:09 +0200

    Auditní log: historie změn (Zmena) v detailu klienta a adminu, log notifikací v adminu

--- klienti/admin.py
+++ klienti/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Klient, HypotekaWorkflow, UserProfile
+from .models import Klient, HypotekaWorkflow, UserProfile, Zmena, NotifikaceLog
 
 @admin.register(Klient)
 class KlientAdmin(admin.ModelAdmin):
@@ -18,3 +18,17 @@ class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'role')
     list_filter = ('role',)
     search_fields = ('user__username',)
+
+@admin.register(Zmena)
+class ZmenaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'klient', 'author', 'created', 'popis')
+    list_filter = ('klient', 'author', 'created')
+    search_fields = ('klient__jmeno', 'author', 'popis')
+    date_hierarchy = 'created'
+
+@admin.register(NotifikaceLog)
+class NotifikaceLogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'prijemce', 'typ', 'klient', 'datum', 'uspesne')
+    list_filter = ('typ', 'uspesne', 'datum')
+    search_fields = ('prijemce', 'klient__jmeno', 'obsah')
+    date_hierarchy = 'datum'
