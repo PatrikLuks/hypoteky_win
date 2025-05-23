a91ba05fae7ed764936c3b1fc3b40ab16e655b9c
Patrik Luks <patrikluks@Patrik--MacBook-Air.local>
2025-05-23 10:57:54 +0200

    Úklid projektu: odstranění zbytečných skriptů a prázdných/dočasných souborů

--- create_userprofiles.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import User
-from klienti.models import UserProfile
-
-# Hromadné vytvoření UserProfile pro všechny uživatele
-for user in User.objects.all():
-    profile, created = UserProfile.objects.get_or_create(user=user)
-    profile.save()
-    print(f"UserProfile for {user.username} ({'created' if created else 'updated'}) - role: {profile.role}")
